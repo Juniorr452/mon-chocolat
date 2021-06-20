d12ade8616c6c04e8256f95280390acc7fe98450
Juniorr452 <juniorr452@gmail.com>
2021-06-20 09:20:43 -0300

    chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "@fontsource/raleway/400.css"
+import "@fontsource/roboto/700.css"
+
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from "../theme"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ theme/index.ts
@@ -0,0 +1,25 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+}
+
+export const theme = extendTheme({
+  colors,
+  fonts: {
+    heading: 'Roboto',
+    body: 'Raleway'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.900",
+        color: "white"
+      }
+    }
+  },
+})
